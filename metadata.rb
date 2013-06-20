8e62edd733ea1c31cd60c09112c5015b178c715d
Francesco Laffi <francesco.laffi@gmail.com>
2013-06-20 19:25:12 +0200

    stricter dependency on apache2 cookbook

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'wpcli'
 maintainer       'Francesco Laffi'
 maintainer_email 'francesco.laffi@gmail.com'
 license          'MIT'
-description      'Installs wpcli'
+description      'Installs wp-cli, install and configure wp environment'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
@@ -13,7 +13,7 @@ depends 'php'
 depends 'git'
 depends 'curl'
 
-suggests 'apache2'
+recommends 'apache2'
 
 # %w{ debian ubuntu centos redhat fedora }.each do |os|
 #   supports os
